222adce6a338bd8b04ff3ddd5649fd1bc03736b7
Robot°16 16 <71988762+blak11@users.noreply.github.com>
2021-07-06 21:33:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -648,4 +648,4 @@ client.on("message", msg => {
     msg.channel.send("🔓 | All Channels Unlocked");
   }
 });
-client.login("ODAzNjUwNDI2NTcwMDE0NzMw.YBA3yA.upFtz2CqedWLJwqZ6yrFYOe6Zf8");
+client.login("ODA2MDc0NjMzMTkwODk5NzMy.YBkJgA.tmzfJbNllG4DKRkGkh4OSMNpeoU");
