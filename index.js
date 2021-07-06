0f93b9820c711fce4b93b233afe5921a54a9e981
Robot°16 16 <71988762+blak11@users.noreply.github.com>
2021-07-07 01:23:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -78,8 +78,7 @@ client.on("message", async message => {
 \`f/stats\` Or \`f/reset\`
 \`f/logs\` : __Someone's ID__
 \`f/antibot\` : __off , On__
-__Brdne bota Boservart__
-__[invite bot]__(https://discord.com/api/oauth2/authorize?client_id=803650426570014730&permissions=8&scope=bot) - __[Website]__(https://fsociety-bot4700.glitch.me) - __[Support]__(https://discord.gg/SEdp93xACp)
+[invite bot](https://discord.com/api/oauth2/authorize?client_id=803650426570014730&permissions=8&scope=bot) - [Website](https://fsociety-bot4700.glitch.me) - [Support](https://discord.gg/SEdp93xACp)
 `);
 
     message.channel.send(help);
