4f87768c7f27c085766a099cc9de46850df36bcb
Tiffany <69997996+Selinvictor@users.noreply.github.com>
2021-08-07 22:53:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -286,36 +286,37 @@ client.on('message', message => {
     }
 });
 
-//client.on("message", msg => {
-//if (!msg.channel.guild)
- //      return msg.channel.send("");
-//  if (msg.author.bot) return;
-//  if (msg.content.includes("@everyone")) {
-//    if (msg.member.hasPermission("MANAGE_MESSAGES")) return;
-//    if (!msg.channel.guild) return;
-//    msg.delete();
-// const here = new Discord.MessageEmbed()
-//      .setColor("#00000")
-//      .setDescription(
- //       `❌ | **Deleted Message**
-// ❯ **Channel Name** : <#${msg.channel.id}>
-// ❯ **Message By** : <@${msg.author.id}>
-// ❯ **Reason** : Send Everyone : ❌
-// `
-//      )
- //   .setThumbnail(msg.author.avatarURL())
-//    .setTimestamp()     
-//.setFooter(`${msg.author.tag}`, msg.author.avatarURL())
-//          .setTimestamp()
+client.on("message", msg => {
+if (!msg.channel.guild)
+       return msg.channel.send("");
+  if (msg.author.bot) return;
+  if (msg.content.includes("@everyone")) {
+   if (msg.member.hasPermission("MANAGE_MESSAGES")) return;
+   if (!msg.channel.guild) return;
+    msg.delete();
+ const here = new Discord.MessageEmbed()
+     .setColor("#00000")
+      .setDescription(
+        `❌ | **Deleted Message**
+ ❯ **Channel Name** : <#${msg.channel.id}>
+ ❯ **Message By** : <@${msg.author.id}>
+ ❯ **Reason** : Send Everyone : ❌
+ `
+      )
+   .setThumbnail(msg.author.avatarURL())
+    .setTimestamp()     
+.setFooter(`${msg.author.tag}`, msg.author.avatarURL())
+          .setTimestamp()
 
-// msg.channel.send(here).then(msg => msg.delete({ timeout: 500 }))
+msg.channel.send(here).then(message => {
+if(message.deletable) message.delete({timeout : 10000})
 })
 
- // 
-//
-//  }
-//  
-//});
+
+  
+}
+  
+});
 // ======== { • anti here • }======== //
 //client.on("message", msg => {
  //if (!msg.channel.guild)
