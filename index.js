28a0ecbbce7fe8411e86f5997a5fa8a4414e058f
Tiffany <69997996+Selinvictor@users.noreply.github.com>
2021-08-08 00:17:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -290,7 +290,7 @@ client.on('message', message => {
 
 client.on("message", msg => {
   if (!msg.channel.guild) return msg.channel.send("");
-  //if (msg.author.bot) return;
+  if (msg.author.bot) return;
   if (msg.content.includes("@everyone")) {
   if (msg.member.hasPermission("MANAGE_MESSAGES")) return;
     msg.delete();
