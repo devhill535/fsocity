2cc5fb699391bfc8e3afec5ce1627b4995cbd521
Tiffany <69997996+Selinvictor@users.noreply.github.com>
2021-08-09 02:59:53 +0300

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,18 +1,26 @@
-const Discord = require('discord.js');
 
-module.exports = {
-	name: "h",
-	description: "Help command default prefix (!)",
-	aliases: ['commands', 'bothelp', 'botinfo'],
-	cooldown: 1,
-	execute(message, args){
-		const helpEmbed = new Discord.MessageEmbed()
-		.setTitle('List of all my commands!')
-		.setThumbnail(message.author.displayAvatarURL({ dyanmic: true }))
-        .setAuthor(`${message.author.tag}`)
-		.setColor('RANDOM')
-		.setTimestamp()
-		.addFields(
+const { Command } = require('discord-akairo');
+
+class RecentCommand extends Command {
+    constructor() {
+        super('logs', {
+            aliases: ['logs'],
+            args: [
+                {
+                    id: 'ID'
+                }
+            ],
+            channel: 'guild'
+        });
+    }
+
+    async exec(message, args) {
+        const embed = this.client.util
+            .embed()
+            .setColor("#00000")
+            .setAuthor(`${message.author.tag}`)
+	    .setTimestamp()
+	    .addFields(
 			{ name: '**!help utility**', value: 'Commands for utility section**', inline: true },
 			{ name: '**!help other**', value: 'Commands for other section', inline: true },
 			{ name: '**!help info**', value: 'Commands for info section', inline: true },
@@ -21,6 +29,5 @@ module.exports = {
 			{ name: '**!help interaction**', value: 'Commands for interaction section', inline: true },
 			{ name: '**!help economy(coming soon)**', value: 'Commands for economy section', inline: true },
 		)
-		message.channel.send(helpEmbed)
-	}
-}
+
+module.exports = RecentCommand;
