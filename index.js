f00bc7dfa16d0bc6dcbf086e702d1c76c7bc1e0f
Tiffany <69997996+Selinvictor@users.noreply.github.com>
2021-08-10 04:28:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ require("events").EventEmitter.defaultMaxListeners = 200;
 ;
 const GuardianClient = require('./core/client.js');
 const client = new GuardianClient();
-//client.on("ready", async () => {
-//  console.log(`${client.user.username} Ready .`);
-  //console.log(`${client.guilds.cache.size} Servers .`);
- // console.log(`${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} Users .`);
-// client.user.setStatus("invisible")
-//  client.user.setActivity("f/help |Fsociety Is Here", {
-  //  type: "PLAYING"
- // });
-//});
 client.on("ready", () => {
   console.log(`Hello ${client.user.username} is now online!`);
+  client.channels.cache.get("874463630832336956").send(`
+ •••••
+❯  **Servers** : ${client.guilds.cache.size}
+•••••
+❯  **Users** : ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}
+•••••
+❯  **Channels** : ${client.channels.cache.size}
+•••••`
+     );
 
   let channel = client.channels.cache.get("848398797729431573");
   if (!channel) return console.error("The channel does not exist!");
