957f59ca3846bd1c6cef9fb96d63a73a0d0993d7
Tiffany <69997996+Selinvictor@users.noreply.github.com>
2021-08-23 03:18:48 +0300

    Update help2.js

--- commands/general/help2.js
+++ commands/general/help2.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const fs = require('fs');
 
 module.exports = {
-  name: 'help2',
+  name: 'help',
   aliases: ('cmds'),
   description: 'Shows you the list of commands.',
   usage: 'help',
@@ -10,18 +10,55 @@ module.exports = {
   async execute(client, message, args) {
 
     const helpEmbed = new Discord.MessageEmbed()
-      .setColor("#de873")
+      .setColor("#de526")
       .setAuthor(`${client.user.username} Discord Bot\n`)
       .setDescription('• 📍 Prefix: ``' + prefix + '``\n' +
         `• 🔧 Developer: ${dev}\n\n⚙️ - **Panel**\n👮 - **Moderation**\n❔ - **Other**`);
         
      const security = new Discord.MessageEmbed()
-      .setColor("#de627")
+      .setColor("#de762")
       .setAuthor(`${client.user.username} Discord Bot\n`)
       .setDescription('• 📍 Prefix: ``' + prefix + '``\n' +
         `• 🔧 Developer: ${dev}\n\n⚙️ - **Panel**\n👮 - **Moderation**\n❔ - **Other**`);
 
     const moderationEmbed = new Discord.MessageEmbed()
-      .setColor("#de536")
+      .setColor("#de245")
       .setAuthor(`Config\n`)
-      .setDescription('To get more 
+      .setDescription('To get more information about a certain command, use ``' + prefix +
+        'help [command]``.\n\n•``test``, ``test2``, ``test3``.');
+
+    try {
+      const filter = (reaction, user) => {
+        return (reaction.emoji.name === '⚙️' || '👮' || '❔' || '📎') && user.id === message.author.id;
+      };
+      message.delete();
+      message.channel.send(helpEmbed).then(embedMsg => {
+        embedMsg.react("⚙️")
+        .then(embedMsg.react("👮"))
+          .then(embedMsg.react("📎"))
+          .then(embedMsg.react("❔"))
+        embedMsg.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+          .then(collected => {
+            const reaction = collected.first();
+
+            if (reaction.emoji.name === '⚙️') {
+              embedMsg.edit(helpEmbed);
+            } else if (reaction.emoji.name === '👮') {
+              embedMsg.edit(moderationEmbed);
+            } else if (reaction.emoji.name === '📎') {
+              embedMsg.edit(security);
+            } else if (reaction.emoji.name === '❔') {
+              message.reply('test.');
+            }
+          })
+          .catch(collected => {
+            message.reply('didnt work.');
+          });
+      });
+
+
+    } catch (e) {
+      console.log(e.stack);
+    }
+  }
+}
