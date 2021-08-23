091e05072196d1777b15ad1a940cc7826656bbb0
Tiffany <69997996+Selinvictor@users.noreply.github.com>
2021-08-23 03:17:02 +0300

    Create help2.js

---
+++ commands/general/help2.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+  name: 'help2',
+  aliases: ('cmds'),
+  description: 'Shows you the list of commands.',
+  usage: 'help',
+  example: 'help',
+  async execute(client, message, args) {
+
+    const helpEmbed = new Discord.MessageEmbed()
+      .setColor(color)
+      .setAuthor(`${client.user.username} Discord Bot\n`)
+      .setDescription('• 📍 Prefix: ``' + prefix + '``\n' +
+        `• 🔧 Developer: ${dev}\n\n⚙️ - **Panel**\n👮 - **Moderation**\n❔ - **Other**`);
+        
+     const security = new Discord.MessageEmbed()
+      .setColor("#de627")
+      .setAuthor(`${client.user.username} Discord Bot\n`)
+      .setDescription('• 📍 Prefix: ``' + prefix + '``\n' +
+        `• 🔧 Developer: ${dev}\n\n⚙️ - **Panel**\n👮 - **Moderation**\n❔ - **Other**`);
+
+    const moderationEmbed = new Discord.MessageEmbed()
+      .setColor("#de536")
+      .setAuthor(`Config\n`)
+      .setDescription('To get more 
