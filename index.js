2c27395b394b1d74664c76ca8e708cf49df445d9
Tiffany <69997996+Selinvictor@users.noreply.github.com>
2021-08-28 01:51:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ client.on("message", async message => {
      
       .setThumbnail(message.author.avatarURL())
       .setTitle(`All Commands Bot Fsociety:`)
-      .setDescription("Here is the list of commands!\nneed more help? Come join your[guild](https://discord.gg/PftVmBUr5X)")
+      .setDescription(`Here is the list of commands!\nneed more help? Come join your[guild](https://discord.gg/PftVmBUr5X)`)
       .addField("**┊Info Commands : {7}**", "`usre, botlist, invite, bots, uptime, messages, vote`")
       .addField("**┊Admin Commands : {11}**", "`lock, unlock, lock all, unlock all, clear, ban, kick, nick, mute, unmute, slowmode`")
       .addField("**┊Security Commands : {7}**", "`settings, anti, log, stats, logs`")
