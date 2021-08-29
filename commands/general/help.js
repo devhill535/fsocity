abc84e28b96a36ffd9346961816e563907da8e5c
Tiffany <69997996+Selinvictor@users.noreply.github.com>
2021-08-29 03:42:58 +0300

    Create help.js

---
+++ commands/general/help.js
@@ -0,0 +1,28 @@
+const { Command } = require('discord-akairo');
+
+class helpCommand extends Command {
+    constructor() {
+        super('1', {
+            aliases: ['2']
+        });
+    }
+  
+  
+  async exec(message) {
+        const embed = new MessageEmbed()
+            .setColor(this.client.color)
+            .setTitle(`${this.client.user.username} information`)
+            .setThumbnail(this.client.avatar)
+            .addField('Created by', `\`${this.client.users.cache.get(this.client.ownerID[0]).tag} (${this.client.ownerID[0]})\``)
+            .addField('Language', '[NodeJS](https://nodejs.org/en/) ([discord.js](https://discord.js.org/#/))')
+            .addField('Framework', '[discord-akairo](https://discord-akairo.github.io/#/)')
+            .addField('What does \'Rada\' mean', 'Rada originates in Slavic languages and means "filled with care". As a feminine given name it is used in Slavic languages, mainly Russian, in the United States it is very rare.')
+            .addField('Color', this.client.color)
+            .addField('GitHub Repository', `[Rada](https://github.com/Iskawo/Rada)`)
+            .setFooter(`Requested by ${message.author.username}`)
+            .setTimestamp()
+        return message.util.send(embed);
+    }
+}
+
+module.exports = helpCommand;
