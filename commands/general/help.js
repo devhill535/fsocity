fe40e98f298b8e80687ab7e0c906337c8d4816ca
Tiffany <69997996+Selinvictor@users.noreply.github.com>
2021-08-29 03:46:43 +0300

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -13,7 +13,7 @@ class LingCommand extends Command {
             .setColor(this.client.color)
             .setTitle(`${this.client.user.username} information`)
             .setThumbnail(this.client.avatar)
-            .addField('Created by', `\`${this.client.users.cache.get(this.client.ownerID[0]).tag} (${this.client.ownerID[0]})\``)
+            .addField('Created by', '@')
             .addField('Language', '[NodeJS](https://nodejs.org/en/) ([discord.js](https://discord.js.org/#/))')
             .addField('Framework', '[discord-akairo](https://discord-akairo.github.io/#/)')
             .addField('What does \'Rada\' mean', 'Rada originates in Slavic languages and means "filled with care". As a feminine given name it is used in Slavic languages, mainly Russian, in the United States it is very rare.')
