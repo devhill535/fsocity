3cb1bd74fa5fd822c424ab4961a9b2022b894b0d
Halat <88115270+Halat69@users.noreply.github.com>
2021-11-13 11:59:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,20 +9,20 @@ module.exports = {
             per_hour: 5
         },
         role_creations: {
-            per_minute: 10,
-            per_hour: 10
+            per_minute: 5,
+            per_hour: 5
         },
         channel_creations: {
-            per_minute: 10,
-            per_hour: 10
+            per_minute: 5,
+            per_hour: 5
         },
         role_deletions: {
-            per_minute: 10,
-            per_hour: 10
+            per_minute: 5,
+            per_hour: 5
         },
         channel_deletions: {
-            per_minute:10,
-            per_hour: 10
+            per_minute:5,
+            per_hour: 5
         },
         unbans: {
             per_minute: 5,
@@ -30,12 +30,12 @@ module.exports = {
         
     },
         emoji_creations: {
-            per_minute: 10,
-            per_hour: 10
+            per_minute: 5,
+            per_hour: 5
         },
         emoji_deletions: {
-            per_minute: 10,
-            per_hour: 10
+            per_minute: 5,
+            per_hour: 5
         }
     },
     
